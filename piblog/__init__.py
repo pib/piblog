76a4b7c46aa3c6b96706b55c6d5416e7ffc03288
Paul Bonser <pib@paulbonser.com>
2013-08-25 22:28:46 -0500

    Allow passing config file path, init app via blueprint

--- piblog/__init__.py
+++ piblog/__init__.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Blueprint, Flask, current_app
 
-app = Flask(__name__)
+piblog_bp = Blueprint('piblog_bp', __name__)
 
 
-@app.route('/')
+@piblog_bp.route('/')
 def index():
-    return '<html><head></head><body>Simple index</body></html>'
+    configured_message = current_app.config.get('INDEX_MESSAGE',
+                                                'Simple Index')
+    return '<html><head></head><body>{}</body></html>'.format(
+        configured_message)
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+    app.register_blueprint(piblog_bp)
+
+    if config is not None:
+        app.config.from_pyfile(config)
+
+    return app
--- piblog/script.py
+++ piblog/script.py
@@ -1,8 +1,9 @@
 from flask_script import Manager
-from . import app
-
-manager = Manager(app)
+from . import create_app
 
 
 def run():
+    manager = Manager(create_app)
+    manager.add_option('-c', '--config', dest='config', required=False,
+                       help='Specify a config to load before running')
     manager.run()
